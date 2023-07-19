7875694c8302df8ef5e56ea704b08a4390f61010
Aakarshit Choudhary <102687269+Aakarshit07@users.noreply.github.com>
2023-07-19 11:25:51 +0530

    Delete MIleStone 1 -Js directory

--- MIleStone 1 -Js/Vowel Counter/script.js
+++
@@ -1,6 +0,0 @@
-// Using RegX
-let string = prompt("Please Enter a Name")
-const reg = /[aeiou]/gi
-let  letters =  string.match(reg);
-document.write(letters.length, "String is: ", string)
-console.log(letters.length, "myString is ", string);
